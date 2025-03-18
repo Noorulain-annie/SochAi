1396cd395f25eba6f4e5ea331794743b8030dfbf
Noorulain <noorulainsaleh19@gmail.com>
2025-03-18 13:10:55 +0500

    login screen done with navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
+
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRouter } from 'expo-router';
 
 const SignupScreen = () => {
+  const router = useRouter();
   return (
     <SafeAreaView style={styles.container}>
       <View>
@@ -50,7 +53,7 @@ const SignupScreen = () => {
         <Text style={{ color: '#AEAEAE' }}>or</Text>
       </View>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => router.push('/LoginScreen')}>
         <View style={{
           display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 20, height: 50, paddingHorizontal: 10,
           paddingVertical: 7, backgroundColor: '#007BFF',
@@ -134,76 +137,3 @@ const styles = StyleSheet.create({
   },
 });
 
-
-// import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-// import React from 'react'
-// import { SafeAreaView } from 'react-native-safe-area-context'
-
-// const SignupScreen = () => {
-//   return (
-//     <SafeAreaView>
-//       {/* logo  */}
-//       <View style={{ display: 'flex', justifyContent: 'center', top: '134', flexDirection: 'row', gap: '10' }}>
-//         <Image source={require('../../assets/images/LoginLogo.png')} />
-//         <Text style={styles.buttonText}>
-//           Soch
-//           <Text style={styles.redText}>Ai</Text>
-//         </Text>
-//       </View>
-//       {/* welcome text */}
-//       <View style={{ display: 'flex', flexDirection: 'column', width: '369', height: '63', top: '170', marginHorizontal: '31', gap: '5' }}>
-//         <Text style={styles.textheading}>Welcome Back!</Text>
-//         <Text style={styles.textsubheading}>Genius say Jugaar tak - Smarter Together</Text>
-//       </View>
-//       {/* login with */}
-//       <View style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
-//         <TouchableOpacity style={{ width: 369, top: 230 }}>
-//           <View style={{ display: 'flex', flexDirection: 'row', gap: 15, justifyContent: 'center', alignItems: 'center' }}>
-//             <Image source={require('../../assets/images/logo_facebook.png')} />
-//             <Text style={{ color: '#383838' }}>Continue with Facebook</Text>
-//           </View>
-//         </TouchableOpacity>
-
-//         <TouchableOpacity style={{ width: 369, top: 230 }}>
-//           <View style={{ display: 'flex', flexDirection: 'row', gap: 15, justifyContent: 'center', alignItems: 'center' }}>
-//             <Image source={require('../../assets/images/logo_facebook.png')} />
-//             <Text style={{ color: '#383838' }}>Continue with Google</Text>
-//           </View>
-//         </TouchableOpacity>
-
-//         <TouchableOpacity style={{ width: 369, top: 230 }}>
-//           <View style={{ display: 'flex', flexDirection: 'row', gap: 15, justifyContent: 'center', alignItems: 'center' }}>
-//             <Image source={require('../../assets/images/logo_facebook.png')} />
-//             <Text style={{ color: '#383838' }}>Continue with Apple</Text>
-//           </View>
-//         </TouchableOpacity>
-//       </View>
-//     </SafeAreaView >
-//   )
-// }
-
-// export default SignupScreen
-
-// const styles = StyleSheet.create({
-//   buttonText: {
-//     color: 'black',
-//     fontSize: 42.85,
-//     fontWeight: '700',
-//   },
-//   redText: {
-//     color: 'red',
-//   },
-//   textheading: {
-//     fontSize: 30.96,
-//     fontWeight: '700',
-//     color: 'black',
-
-//   },
-//   textsubheading: {
-//     color: '#AEAEAE',
-//     width: 309,
-//     height: 25,
-//     fontSize: 15.96,
-//     fontWeight: '400',
-//   }
-// })
